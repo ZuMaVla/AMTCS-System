affb4ae29291f68a482516e8a40fe018826d0f09
ZuMaVla <marinazubialevich@gmail.com>
2026-08-23 18:32:02 +0100

    Add text version of expirement status, tune visual appearence of the remote control client's dashboard

--- rc_client_browser/view/rc_view.js
+++ rc_client_browser/view/rc_view.js
@@ -15,12 +15,21 @@ export class RemoteControlView {
     updateServerStatus(status) {
         document.getElementById("server-status").textContent = status;
     }
+
     getAccessCode() {
         return document.getElementById("access-code").value.trim();
     }
 
     updateExperimentStatus(status) {
-        document.getElementById("experiment-status").textContent = status;
+        let text_exp_status = "Unknown";
+        switch (status) {
+            case -1: text_exp_status = "Not started"; break;
+            case 0:  text_exp_status = "Unknown"; break;
+            case 1:  text_exp_status = "Running"; break;
+            case 2:  text_exp_status = "Paused"; break;
+            case 3:  text_exp_status = "Finished"; break;
+        }
+        document.getElementById("experiment-status").textContent = text_exp_status;
     }
 
     renderLogs(logList) {
